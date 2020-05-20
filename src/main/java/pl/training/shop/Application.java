dc6876014c61de281c93160db29a4b367268a84d
landrzejewski <landrzejewski.poczta@gmail.com>
2020-05-20 21:44:21 +0200

    Refactorings

--- src/main/java/pl/training/shop/Application.java
+++ src/main/java/pl/training/shop/Application.java
@@ -1,25 +1,17 @@
 package pl.training.shop;
 
 import lombok.extern.java.Log;
-import org.javamoney.moneta.FastMoney;
 import pl.training.shop.payments.FakePaymentService;
+import pl.training.shop.payments.LocalMoney;
 import pl.training.shop.payments.PaymentRequest;
-
-import javax.money.CurrencyUnit;
-import javax.money.Monetary;
-import java.util.Locale;
 
 @Log
 public class Application {
 
-    private static final Locale LOCALE = Locale.getDefault();
-    private static final CurrencyUnit CURRENCY_UNIT = Monetary.getCurrency(LOCALE);
-    private static final FastMoney MONEY = FastMoney.of(1_000, CURRENCY_UNIT);
-
     public static void main(String[] args) {
         var paymentService = new FakePaymentService();
         var paymentRequest = PaymentRequest.builder()
-                .money(MONEY)
+                .money(LocalMoney.of(1_000))
                 .build();
         var payment = paymentService.process(paymentRequest);
         log.info(payment.toString());
