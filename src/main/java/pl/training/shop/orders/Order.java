a75a132f76a2228ccc7e52730992379ba586d39d
landrzejewski <landrzejewski.poczta@gmail.com>
2020-06-30 11:29:22 +0200

    AOP

--- src/main/java/pl/training/shop/orders/Order.java
+++ src/main/java/pl/training/shop/orders/Order.java
@@ -9,6 +9,7 @@ import pl.training.shop.payments.LocalMoney;
 import pl.training.shop.payments.Payment;
 import pl.training.shop.products.Product;
 
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -17,6 +18,7 @@ import java.util.List;
 public class Order {
 
     private Long id;
+    @NotEmpty
     @NonNull
     private List<Product> products;
     private Payment payment;
